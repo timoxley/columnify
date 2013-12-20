297198c4e6591b8da55fbda9e1a94ebf72587ca6
Tim Oxley <secoif@gmail.com>
2013-12-21 08:45:37 +1000

    Refactor utilities to utils for testability.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 "use strict"
 
+var utils = require('./utils')
+var padRight = utils.padRight
+var splitIntoLines = utils.splitIntoLines
+var splitLongWords = utils.splitLongWords
+
 module.exports = function(items, options) {
 
   // defaults
@@ -154,70 +159,4 @@ function createRows(items, columns, columnNames) {
     }
     return row
   })
-}
-
-/**
- * Pad `str` up to total length `max` with `chr`.
- * If `str` is longer than `max`, padRight will return `str` unaltered.
- *
- * @param String str string to pad
- * @param Number max total length of output string
- * @param String chr optional. Character to pad with. default: ' '
- * @return String padded str
- */
-
-function padRight(str, max, chr) {
-  var length = 1 + max - str.length
-  if (length <= 0) return str
-  return str + Array.apply(null, {length: length})
-  .join(chr || ' ')
-}
-
-/**
- * Split a String `str` into lines of maxiumum length `max`.
- * Splits on word boundaries.
- *
- * @param String str string to split
- * @param Number max length of each line
- * @return Array Array containing lines.
- */
-
-function splitIntoLines(str, max) {
-  return str.trim().split(' ').reduce(function(lines, word) {
-    var line = lines[lines.length - 1]
-    if (line && line.join(' ').length + word.length < max) {
-      lines[lines.length - 1].push(word) // add to line
-    }
-    else lines.push([word]) // new line
-    return lines
-  }, []).map(function(l) {
-    return l.join(' ')
-  })
-}
-
-/**
- * Add spaces and `truncationChar` between words of
- * `str` which are longer than `max`.
- *
- * @param String str string to split
- * @param Number max length of each line
- * @param Number truncationChar character to append to split words
- * @return String
- */
-
-function splitLongWords(str, max, truncationChar, result) {
-  result = result || []
-  if (!str) return result.join(' ') || ''
-  var words = str.split(' ')
-  var word = words.shift() || str
-
-  if (word.length > max) {
-    var remainder = word.slice(max - truncationChar.length) // get remainder
-    words.unshift(remainder) // save remainder for next loop
-
-    word = word.slice(0, max - truncationChar.length) // grab truncated word
-    word += truncationChar // add trailing … or whatever
-  }
-  result.push(word)
-  return splitLongWords(words.join(' '), max, truncationChar, result)
 }
