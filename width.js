4b4934ed20f55b89c4cd090dee18e957dbd17490
Tim Oxley <secoif@gmail.com>
2016-01-06 06:07:50 +0800

    Revert "Chunk calls to stripAnsi to avoid blowing max string length on regex."
    Not necessary now that splitLongWords won't blow stack.
    
    This reverts commit fd6a0c9f04fa0e247b82a86b85ee99d4054690b3.

--- width.js
+++ width.js
@@ -1,15 +1,6 @@
 var stripAnsi = require('strip-ansi')
 var wcwidth = require('wcwidth')
 
-var stepSize = 10000
 module.exports = function(str) {
-  // step through data in increments as the stripAnsi
-  // regex is sensitive to large strings.
-  var width = 0
-  for (var i = 0; i < str.length; i += stepSize + 1) {
-    width += wcwidth(stripAnsi(
-      str.slice(i, i + stepSize)
-    ))
-  }
-  return width
+  return wcwidth(stripAnsi(str))
 }
